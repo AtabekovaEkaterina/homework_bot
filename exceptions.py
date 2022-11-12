dcc89cbfa60f9a30a753d36d0b9082f13fc91b57
Екатерина Атабекова <katerina19870405@yandex.ru>
2022-11-12 11:34:04 +0300

    description correction

--- exceptions.py
+++ exceptions.py
@@ -2,9 +2,3 @@ class HTTPErrorException(Exception):
     """Создание собственного исключения, если статус HTTP не 200."""
 
     pass
-
-
-class TokenErrorException(Exception):
-    """Создание собственного исключения, если статус HTTP не 200."""
-
-    pass
